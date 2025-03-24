5f7c766a5ec648053f6ba3547a7c036ab0cc6df1
stephmukami <stephmukami@gmail.com>
2025-03-23 18:02:36 -0700

    fix:remove typos on navbar

--- map-my-wheels/src/components/Navbar.jsx
+++ map-my-wheels/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='bg-brand-green  text-center p-4 md:h-[150px] h-[100px]'>
-    <h4 className='text-white text-3xl mb-2'>        Power Up by Greenwheels
+    <h4 className='text-white text-3xl mb-2'>        Power Up
     </h4>
     <p className= "text-white">
       Pins of EV Swapping stations
